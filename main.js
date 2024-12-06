37402861420b134e2b9f29189ff5ad457cf3cd65
paul schuhmacher <contact@pschuhmacher.com>
2024-12-06 16:34:07 +0100

    minor feat

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const { app, BrowserWindow } = require('electron')
 //@See : https://www.electronforge.io/import-existing-project#adding-squirrel.windows-boilerplate
 if (require('electron-squirrel-startup')) app.quit();
 
-const { updateElectronApp, UpdateSourceType } = require('update-electron-app')
-updateElectronApp()
+// const { updateElectronApp, UpdateSourceType } = require('update-electron-app')
+// updateElectronApp()
 
 const createWindow = () => {
   const win = new BrowserWindow({
