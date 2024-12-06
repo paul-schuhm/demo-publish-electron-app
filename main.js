0e59d40cf178648c628018e81febc6ee16ab53a5
paul schuhmacher <contact@pschuhmacher.com>
2024-12-06 09:55:58 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron')
+
+//Pour distribution avec Squirrel.Windows
+//@See : https://www.electronforge.io/import-existing-project#adding-squirrel.windows-boilerplate
+if (require('electron-squirrel-startup')) app.quit();
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
